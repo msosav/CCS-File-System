1793a700a396361a18e6cf2e6e2b335b25471981
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-25 12:44:20 -0500

    Implement partition distribution between active datanodes

--- Proyecto 1/NameNode/namenode.py
+++ Proyecto 1/NameNode/namenode.py
@@ -4,6 +4,39 @@ NameNode
 
 # Diccionario que almacena los datanodes y los archivos que contienen
 datanodes = {}
+active_datanodes = []
+files = {}
+
+
+def distribute_file(file_name, num_partitions, size):
+    """
+    Distribuye un archivo en los datanodes
+    param file_name: El nombre del archivo
+    param num_partitions: La lista de partes del archivo
+    return: El diccionario de datanodes
+    """
+    datanodes[file_name] = {}
+    datanode_index = 0
+    for i in range(num_partitions):
+        if datanode_index >= len(active_datanodes):
+            datanode_index = 0
+        datanodes[file_name][partition_name(
+            i)] = [active_datanodes[datanode_index]]
+        datanode_index += 1
+    files[file_name] = size
+    return datanodes[file_name]
+
+
+def partition_name(i):
+    """
+    Genera el nombre de la partición
+    param i: El número de la partición
+    return: El nombre de la partición
+    """
+    length = len(str(i))
+    serial = '0' * (4 - length) + str(i)
+    name = f"part-{serial}"
+    return name
 
 
 def locate_file(file_name):
@@ -18,12 +51,5 @@ def locate_file(file_name):
 
 
 if __name__ == "__main__":
-    datanodes = {
-        "file1": {
-            "part-0000": ["Datanode1"],
-            "part-0001": ["Datanode2"]
-        },
-        "file2": ["Datanode1"],
-        "file3": ["Datanode2"]
-    }
-    print(locate_file("file1"))
+    active_datanodes = ["Datanode1", "Datanode2"]
+    print(distribute_file("file4", 3, 3000))
